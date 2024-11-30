a95deecff4b9e85c0177417fb05468a89d7e63ed
mamillagowtham <mamillagowtham@gmail.com>
2024-11-30 23:30:49 +0530

    m ui

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from "@mui/material/Button";
+import Dice from './Dice';
+
+const App = () => {
+  return (
+    <>
+    <Button variant = "contained" sx={{color: "red" , border:  "20px solid grey" ,typography: "h2"}}>App</Button>
+    <Button disabled sx={{ ".Mui-disabled" : {color : "red"}}}> click me </Button>
+    <Dice  roll ="6"/>
+    </> 
+  )
+}
+
+export default App
---
+++ my-vue-app/src/Dice.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dice = (props) => {
+  return (
+    <div>{props.roll}</div>
+  )
+}
+
+export default Dice
